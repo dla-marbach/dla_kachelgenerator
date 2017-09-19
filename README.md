ca3e3fc08435bed74647b664fbe574f5b9ee8d98
Alexander Harm <Alexander.Harm@dla-marbach.de>
2017-09-19 12:49:26 +0200

    Reimplementierung der Rekursivität sowie Änderungen in der Erstellung der JSON Datei für bessere Kompatibilität mit dem dla_kachelviewer

--- README.md
+++ README.md
@@ -15,7 +15,9 @@ und generiert die Kacheln mit dem zuerst gefundenen Tool.
 
 Siehe `INSTALL.md`.
 
-### Benutzung
+### Beschreibung
+
+Der DLA Kachelgenerator generiert DeepZoomImages aus allen Dateien in einem übergebenen Verzeichnis, bzw. allen Dateien in spezifischen Unterverzeichnissen. Zusätzlich wird eine JSON-Datei im `tiles`-Ordner erzeugt, die die komplette Dateiliste zur Verwendung mit z. B. [OpenSeadragon](https://openseadragon.github.io/) enthält. Die JSON-Datei ist nach dem übergeordneten Verzeichnis benannt.
 
 #### Hilfe
 
@@ -24,18 +26,28 @@ kachelgenerator.py -h
 ```
 
 ```
-usage: kachelgenerator_v3.py [-h] [-l] [-n] [-w [WORKER]] [paths [paths ...]]
+usage: kachelgenerator.py [-h] 
+                          [--recursive [RECURSIVE [RECURSIVE ...]]]
+                          [--extensions [EXTENSIONS [EXTENSIONS ...]]] 
+                          [-l]
+                          [-n] 
+                          [-w [WORKER]]
+                          [paths [paths ...]]
 
 DeepZoomImages-Generator
 
 positional arguments:
-  paths        Pfade, die die Digitalisate enthalten.
+  paths                 Pfade, die die Digitalisate enthalten.
 
 optional arguments:
-  -h, --help   show this help message and exit
-  -l           Erstellt Logfile (kachelgenerator.log).
-  -n           Erstellt Networker Direktive (tiles-Ordner werden vom Backup ausgeschlossen).
-  -w [WORKER]  Anzahl Worker (default 4).
+  -h, --help            show this help message and exit
+  --recursive [RECURSIVE [RECURSIVE ...]]
+                        Ordner, die die Digitalisate enthalten (absteigende Präferenz)
+  --extensions [EXTENSIONS [EXTENSIONS ...]]
+                        Mögliche Dateiendungen der Digitalisate (default: tiff, tif, jpg, jpeg)
+  -l                    Erstellt Logfile (kachelgenerator.log).
+  -n                    Erstellt Networker Direktive (tiles-Ordner werden vom Backup ausgeschlossen).
+  -w [WORKER]           Anzahl Worker (default 4).
 ```
 
 #### Beispiele
@@ -45,6 +57,12 @@ optional arguments:
 
 * in den übergebenen Verzeichnissen
 `./kachelgenerator.py /pfad/1 /pfad/2`
+
+* in den Ordnern "org" und "max" innerhalb der übergebenen Verzeichnisse
+`./kachelgenerator.py /pfad/1 /pfad/2 --recursive org max`
+
+* nur Dateien mit der Endung "tiff"
+`./kachelgenerator.py --extensions .tiff`
 
 * ohne Multiprocessing
 `./kachelgenerator.py -w 1`
@@ -59,12 +77,4 @@ optional arguments:
 `./kachelgenerator.py -n`
 
 * volles Programm
-`./kachelgenerator.py -l —n -w 8 /pfad/1 /pfad/2`
-
-### Konfiguration
-
-Das Skript konvertiert standardmässig folgende Dateiendungen:
-
-`dateiendungen = ('.tiff', '.tif', '.jpg', '.jpeg')`
-
-Dise Liste wird als Tuple angegeben und kann entsprechend angepasst werden.
+`./kachelgenerator.py /pfad/1 /pfad/2 --recursive org max  --extensions .tiff -l —n -w 8`
--- kachelgenerator.py
+++ kachelgenerator.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # Skript zum generieren von DeepZoomImages
-# Stand: 07.07.2017
-
-# Mögliche Dateiendungen der zu konvertierenden Dateien
-dateiendungen = ('.tiff', '.tif', '.jpg', '.jpeg')
+# Stand: 19.09.2017
 
 ###############################################################################
 
@@ -12,6 +9,7 @@ dateiendungen = ('.tiff', '.tif', '.jpg', '.jpeg')
 import argparse, errno, json, logging, os, shutil, sys
 from time import time
 from multiprocessing import Pool
+from functools import partial
 
 ###############################################################################
 
@@ -19,6 +17,10 @@ from multiprocessing import Pool
 parser = argparse.ArgumentParser(description='DeepZoomImages-Generator')
 # Ordner, die die zu konvertierenden Dateien enthalten
 parser.add_argument('paths', nargs='*', default=[os.path.curdir], help='Pfade, die die Digitalisate enthalten.')
+# Rekursives Durchsuchen der übergebenen Pfade
+parser.add_argument('--recursive', nargs='*', type=str, help='Ordner, die die Digitalisate enthalten (absteigende Präferenz)')
+# Dateiendungen
+parser.add_argument('--extensions', nargs='*', type=str, help='Mögliche Dateiendungen der Digitalisate (default: tiff, tif, jpg, jpeg)')
 # Logdatei erstellen
 parser.add_argument('-l', dest='logfile', action='store_true', default=False, help='Erstellt Logfile (kachelgenerator.log).')
 # Ausschluss vom Networker Backup
@@ -48,73 +50,65 @@ if args.logfile:
   logger.addHandler(fh)
 
 ###############################################################################
+#
+# Testen, welches Tool installiert ist
+#
+###############################################################################
 
-# Testen, welches Tool installiert ist:
+def check_tool():
 
-# Globale Variable
-tool = 0
+  tool = 0
 
-# 1. VIPS mit Python Bindings
-try:
-  # Import von VIPS zur Erzeugung der DeepZoomImages
-  import gi
-  gi.require_version('Vips', '8.0')
-  from gi.repository import Vips
-  Vips
-except:
-  logger.info('VIPS mit Python-Bindings nicht gefunden.')
-else:
-  tool = 1
-  logger.info('VIPS mit Python-Bindings installiert.')
-  # Concurrency mit Python verwalten
-  os.environ['VIPS_CONCURRENCY'] = '1'
-
-# 2. VIPS als Subprozess
-if tool == 0:
+  # 1. VIPS mit Python Bindings
   try:
-    # Import von subprocess
-    import subprocess
-    subprocess.check_output('vips')
+    # Import von VIPS zur Erzeugung der DeepZoomImages
+    import gi
+    gi.require_version('Vips', '8.0')
+    from gi.repository import Vips
+    Vips
   except:
-    logger.info('VIPS nicht gefunden.')
+    logger.info('VIPS mit Python-Bindings nicht gefunden.')
   else:
-    tool = 2
-    logger.info('VIPS installiert.')
+    tool = 1
+    logger.info('VIPS mit Python-Bindings installiert.')
     # Concurrency mit Python verwalten
     os.environ['VIPS_CONCURRENCY'] = '1'
 
-# 3. DeepZoom
-if tool == 0:
-  try:
-    # Import von deepzoom
-    import deepzoom
-    deepzoom
-  except:
-    logger.info('DeepZoom nicht gefunden.')
-  else:
-    tool = 3
-    logger.info('DeepZoom installiert.')
+  # 2. VIPS als Subprozess
+  if tool == 0:
+    try:
+      # Import von subprocess
+      import subprocess
+      subprocess.check_output('vips')
+    except:
+      logger.info('VIPS nicht gefunden.')
+    else:
+      tool = 2
+      logger.info('VIPS installiert.')
+      # Concurrency mit Python verwalten
+      os.environ['VIPS_CONCURRENCY'] = '1'
 
-# Skript ggfls. terminieren
-if tool == 0:
-  sys.exit('Kein Tool gefunden/installiert! Bitte VIPS oder DeepZoom installieren.')
+  # 3. DeepZoom
+  if tool == 0:
+    try:
+      # Import von deepzoom
+      import deepzoom
+      deepzoom
+    except:
+      logger.info('DeepZoom nicht gefunden.')
+    else:
+      tool = 3
+      logger.info('DeepZoom installiert.')
+
+  return tool
 
 ###############################################################################
-
-# Globale Variablen
-
-# Zählen von Verarbeitungsdauer, Dateien, Fehlern
-bm_time = time()
-bm_files = 0
-bm_errors = 0
-
-# Dateiliste eines zu verarbeitenden Ordners
-filelist = []
-
-###############################################################################
-
+#
 # Kacheln generieren
-def generate_tile(task):
+#
+###############################################################################
+
+def generate_tile(task, tool):
 
   global bm_errors
 
@@ -124,6 +118,9 @@ def generate_tile(task):
   # 1. VIPS Python-Bindings
   if tool == 1:
     try:
+      import gi
+      gi.require_version('Vips', '8.0')
+      from gi.repository import Vips
       img = Vips.Image.new_from_file(filein)
       # img.dzsave(output_dir, layout='zoomify', suffix='.jpg[Q=100]')
       img.dzsave(fileout)
@@ -163,15 +160,56 @@ def generate_tile(task):
     sys.exit()
 
 ###############################################################################
+#
+# Ordner mit den zu konvertierenden Dateien bestimmen
+#
+###############################################################################
 
-# Schleife über die verschiedenen Pfade
-for path in args.paths:
+def determine_input_dirs(path_list, dir_list):
 
-  # Rekursives Durchsuchen der Pfade
-  for rootpath, dirnames, filenames in os.walk(path):
+  dirs = []
+
+  # Schleife über die verschiedenen Pfade
+  for path in path_list:
+
+    # Rekursives Durchsuchen?
+    if dir_list:
+
+      # Rekursives Durchsuchen der Pfads
+      for rootpath, dirnames, filenames in os.walk(path):
+
+        # Prüfen ob der Pfad einen gesuchten Ordnernamen enthält, aber keine weiteren Unterordner
+        for dir in dir_list:
+
+          if rootpath.lower().endswith(dir.lower()) and len(dirnames) == 0:
+
+            dirs.append(rootpath)
+
+    else:
+
+      dirs.append(path)
+
+  return dirs
+
+###############################################################################
+#
+# Ordner mit den zu konvertierenden Dateien verarbeiten
+#
+###############################################################################
+
+def convert_images(dir_list, tool, args):
+
+  global bm_files
+
+  # Mögliche Dateiendungen der zu konvertierenden Dateien
+  file_extensions = ('.tiff', '.tif', '.jpg', '.jpeg')
+  if args.extensions:
+    file_extensions = tuple(args.extensions)
+
+  for dir in dir_list:
 
     # Bestimmung des 'tiles'-Ordners im übergeordneten Ordner
-    tilesfolder = os.path.join(os.path.abspath(os.path.join(rootpath, os.pardir)), 'tiles')
+    tilesfolder = os.path.join(os.path.abspath(os.path.join(dir, os.pardir)), 'tiles')
 
     # Löschen des 'tiles'-Ordners und ggfls. Unterordner
     if os.path.exists(tilesfolder):
@@ -182,7 +220,7 @@ for path in args.paths:
       except:
         raise
 
-    # Erstellen des 'tiles'-Ordners und ggfls. Unterordner
+    # Erstellen des 'tiles'-Ordners
     try:
       os.makedirs(tilesfolder)
     except OSError, e:
@@ -193,44 +231,73 @@ for path in args.paths:
     except:
       raise
 
-    # Zurücksetzen der Listen
-    del filelist[:]
-    tasklist = []
-    
-    # Verarbeiten aller Dateien
-    for filename in filenames:
-
-      # Beschränkung auf Dateiendungen
-      if filename.endswith(dateiendungen):
-
-        # Datei in Listen speichern
-        filelist.append(filename)
-        tasklist.append((filename, os.path.join(rootpath, filename), os.path.join(tilesfolder, filename[:filename.rindex('.')])))
-        bm_files += 1
-
-    # Generieren von DeepZoomImages auslagern in Workers
-    p = Pool(args.worker)
-    p.map(generate_tile, tasklist)
-
-    # Dateiendungen in .dzi ändern
-    for filename in filelist:
-      filename = filename[:filename.rindex('.')] + '.dzi'
-
-    # Speichern der Dateiliste in einer JSON-Datei
-    with open(os.path.join(tilesfolder, filename[:filename.rindex('_')] + '.json'), 'w') as jsonfile:
-      json.dump({'files': filelist}, jsonfile)
-
     # Erstellen der Networker Direktive
     if args.networker:
       if not os.path.exists(os.path.join(tilesfolder[:tilesfolder.rindex('tiles')], 'tiles', '.nsr')):
         with open(os.path.join(tilesfolder[:tilesfolder.rindex('tiles')], 'tiles', '.nsr'), 'w') as networkerfile:
           networkerfile.write('+skip: *')
           logger.info('Networker Direktive erstellt: ' + os.path.join(tilesfolder[:tilesfolder.rindex('tiles')], 'tiles', '.nsr'))
-          
-    # Rekursives Durchsuchen der Ordner verhindern 
-    break
+
+    # Erstellung einer Datei- und Aufgabenliste
+    file_list = []
+    task_list = []
+
+    # Bestimmung der Digitalisate im Pfad
+    for rootpath, dirnames, filenames in os.walk(dir):
+
+      # Erstellen der Dateiliste
+      for filename in filenames:
+
+        # Beschränkung auf Dateiendungen
+        if filename.endswith(file_extensions):
+
+          # Datei in Listen speichern
+          file_list.append(filename)
+          task_list.append((filename, os.path.join(rootpath, filename), os.path.join(tilesfolder, filename[:filename.rindex('.')])))
+          bm_files += 1   
+
+
+      # Wir wollen nur die Digitalisate auf der oberen Ebene
+      break
+
+    # Dateiendungen in .dzi ändern
+    for f in file_list:
+      f = f[:f.rindex('.')] + '.dzi'
+
+    # Speichern der Dateiliste in einer JSON-Datei
+    with open(os.path.join(tilesfolder, os.path.basename(os.path.normpath(tilesfolder.rsplit('/',1)[0])) + '.json'), 'w') as jsonfile:
+      json.dump({'files': file_list}, jsonfile)
+
+    # Generieren von DeepZoomImages auslagern in Workers
+    p = Pool(args.worker)
+    p.map(partial(generate_tile, tool=tool), task_list)
 
 ###############################################################################
+
+# Zählen von Verarbeitungsdauer, Dateien, Fehlern
+bm_time = time()
+bm_files = 0
+bm_errors = 0
+
+# Tool prüfen und ggfls terminieren
+tool = check_tool()
+if tool == 0:
+  sys.exit('Kein Tool gefunden/installiert! Bitte VIPS oder DeepZoom installieren.')
+elif tool == 1:
+  import gi
+  gi.require_version('Vips', '8.0')
+  from gi.repository import Vips
+elif tool == 2:
+  import subprocess
+  subprocess.check_output('vips')
+elif tool == 3:
+  import deepzoom
+
+# Inputorder bestimmen
+input_dirs = determine_input_dirs(args.paths, args.recursive)
+
+# Konvertierung starten
+convert_images(input_dirs, tool, args)
 
 # Stats ausgeben
 print '{0} Sekunden für {1} Dateien mit {2} Fehlern.'.format(time() - bm_time, bm_files, bm_errors)
